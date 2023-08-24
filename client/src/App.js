84911836ab996b3b48ea2cc9b44522247466ed50
naveenkumarph666 <132120677+naveenkumarph666@users.noreply.github.com>
2023-08-24 13:27:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,16 +12,16 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/Register' element={<Register />} />
-        <Route path='/Dashboard' element={<Dashboard />} />
-        <Route path='/myprofile' element={<Myprofile />} />
-        <Route path='/viewmore/:imagePath/:Model/:Price/:RAM/:ROM/:Camera/:Processor/:Ratings/:id/:description' element={<ViewMore />} />
-        <Route path='/viewcart' element={<ViewCart />} />
-        <Route path='/favorites' element={<Favorites />} />
-        <Route path='/orders' element={<Order />} />
-        </Routes>
+      <Routes>
+       <Route path='/' element={<Login />} />
+       <Route path='/Register' element={<Register />} />
+       <Route path='/Dashboard' element={<Dashboard />} />
+       <Route path='/myprofile' element={<Myprofile />} />
+       <Route path='/viewmore/:imagePath/:Model/:Price/:RAM/:ROM/:Camera/:Processor/:Ratings/:id/:description' element={<ViewMore />} />
+       <Route path='/viewcart' element={<ViewCart />} />
+       <Route path='/favorites' element={<Favorites />} />
+       <Route path='/orders' element={<Order />} />
+      </Routes>
       </BrowserRouter>
     </div>
   );
