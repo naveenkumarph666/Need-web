8e2fdeb8143a70469f626387c4986f44b49162bc
naveenkumarph666 <132120677+naveenkumarph666@users.noreply.github.com>
2023-08-24 13:29:20 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,18 @@ import Register from './Register';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-       <Route path='/' element={<Login />} />
-       <Route path='/Register' element={<Register />} />
-       <Route path='/Dashboard' element={<Dashboard />} />
-       <Route path='/myprofile' element={<Myprofile />} />
-       <Route path='/viewmore/:imagePath/:Model/:Price/:RAM/:ROM/:Camera/:Processor/:Ratings/:id/:description' element={<ViewMore />} />
-       <Route path='/viewcart' element={<ViewCart />} />
-       <Route path='/favorites' element={<Favorites />} />
-       <Route path='/orders' element={<Order />} />
-      </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+    <Routes>
+     <Route path='/' element={<Login />} />
+     <Route path='/Register' element={<Register />} />
+     <Route path='/Dashboard' element={<Dashboard />} />
+     <Route path='/myprofile' element={<Myprofile />} />
+     <Route path='/viewmore/:imagePath/:Model/:Price/:RAM/:ROM/:Camera/:Processor/:Ratings/:id/:description' element={<ViewMore />} />
+     <Route path='/viewcart' element={<ViewCart />} />
+     <Route path='/favorites' element={<Favorites />} />
+     <Route path='/orders' element={<Order />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
